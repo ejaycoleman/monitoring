3a49623c54119b071b161527e4d7b4f09457b110
Elliott Coleman <ejaycoleman@googlemail.com>
2020-05-05 08:30:19 +0100

    automatic nav updates

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,13 @@ import 'antd/dist/antd.css';
 import App from './App'
 import Login from './Login/index'
 import './index.css';
-import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
-import { Menu, Button } from 'antd';
+import { BrowserRouter as Router, Route, NavLink, Redirect } from "react-router-dom";
+import { Button } from 'antd';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { ApolloProvider } from 'react-apollo';
 import { setContext } from 'apollo-link-context'
-
 
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('token')
@@ -43,12 +42,12 @@ class AppRouter extends React.Component {
 
   handleClick = (e) => {
     this.setState({
-      current: e.key,
+      current: e.key === "" ? "home" : e.key,
     });
   }
 
-  async componentDidMount() {
-    this.setState({current: window.location.pathname.replace(/\//g, '') === ''? 'home' : window.location.pathname.replace(/\//g, '')})
+  componentDidMount() {
+    this.setState({current: window.location.pathname.replace(/\//g, '') === '' ? 'home' : window.location.pathname.replace(/\//g, '')})
   }
 
   signOut = () => {
@@ -60,23 +59,13 @@ class AppRouter extends React.Component {
     return (
       <Router>
         <div>
-          <Menu
-            onClick={this.handleClick}
-            selectedKeys={[this.state.current]}
-            mode="horizontal"
-          >
-            <Menu.Item key="home">
-              <Link to="/">Home</Link>
-            </Menu.Item>
+            <NavLink exact={true} activeStyle={{fontWeight: "bold", color: "red"}} to='/'>Home</NavLink>
             { 
               localStorage.getItem("AUTH_TOKEN") ?
-                <Button type="danger" onClick={() => this.signOut()}>Sign Out</Button>
+              <Button type="danger" onClick={() => this.signOut()}>Sign Out</Button>
               :
-                <Menu.Item key="login">
-                  <Link to="/login">Login</Link>
-                </Menu.Item>
+              <NavLink exact={true} activeStyle={{fontWeight: "bold", color: "red"}} to='/login'>Login</NavLink>
             }
-          </Menu>
           <Route path="/" exact component={App} />
           <Route path="/login/" component={Login} />
         </div>
