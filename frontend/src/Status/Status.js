dbb3e0c2f093ff72d9639da2da2dae02c9a81859
Elliott Coleman <ejaycoleman@googlemail.com>
2020-05-11 10:19:05 +0100

    add table and json view components

--- frontend/src/Status/Status.js
+++ frontend/src/Status/Status.js
@@ -1,5 +1,14 @@
 import React from 'react'
 import moment from 'moment'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import CancelIcon from '@material-ui/icons/Cancel';
 
 class Status extends React.Component {
     constructor(props) {
@@ -26,11 +35,45 @@ class Status extends React.Component {
         }
     }
 
+    componentDidMount() {
+        this.props.tasks && this.props.tasks.forEach(task => {
+            const hasRanInTime = this.state.ranInTime
+            hasRanInTime[task.number] = false
+            task.executions.forEach(execution => {
+                if (!moment().startOf('day').subtract(task.frequency, task.period).isAfter(moment.unix(execution.datetime))) {
+                    hasRanInTime[task.number] = true
+                    return
+                } 
+            })        
+            this.setState({ranInTime: hasRanInTime})        
+        })
+    }
+  
+
     render() {
         return (
             <div>
                 <h2 className="mv3">Status</h2>
-                <h2>{JSON.stringify(this.state.ranInTime)}</h2>
+                <TableContainer component={Paper}>
+                <Table aria-label="simple table">
+                    <TableHead>
+                    <TableRow>
+                        <TableCell>Test</TableCell>
+                        <TableCell align="right">Last Ran</TableCell>
+                    </TableRow>
+                    </TableHead>
+                    <TableBody>
+                    {Object.keys(this.state.ranInTime).map((row) => (
+                        <TableRow key={row.name}>
+                        <TableCell component="th" scope="row">
+                            {row}
+                        </TableCell>
+                        <TableCell align="right">{this.state.ranInTime[row] ? <CheckCircleIcon style={{color: 'green'}}/> : <CancelIcon style={{color: 'red'}}/> }</TableCell>
+                        </TableRow>
+                    ))}
+                    </TableBody>
+                </Table>
+                </TableContainer>
             </div>
         ) 
     }
--- frontend/src/Upload/Upload.js
+++ frontend/src/Upload/Upload.js
@@ -1,4 +1,25 @@
 import React, { useState } from 'react'
+import JSONTree from 'react-json-tree'
+
+const theme = {
+	scheme: 'monokai',
+	base00: '#292C33',
+	base01: '#383830',
+	base02: '#49483e',
+	base03: '#75715e',
+	base04: '#a59f85',
+	base05: '#f8f8f2',
+	base06: '#f5f4f1',
+	base07: '#f9f8f5',
+	base08: '#f92672',
+	base09: '#fd971f',
+	base0A: '#f4bf75',
+	base0B: '#a6e22e',
+	base0C: '#a1efe4',
+	base0D: '#66d9ef',
+	base0E: '#ae81ff',
+	base0F: '#cc6633'
+};
 
 const Upload = props => {
     const [ tasks, setTasks ] = useState("");		
@@ -14,9 +35,7 @@ const Upload = props => {
 			/>
 			<button onClick={() => props.uploadMutation({ tasks }).catch(e => {console.log(e)})}>UPLOAD</button>
 			</div>
-            <h2>
-                {JSON.stringify(props.tasks)}
-            </h2>
+			<JSONTree data={props.tasks} theme={theme} invertTheme={false} shouldExpandNode={()=>true}/>
 		</div>
 	) 
 }
