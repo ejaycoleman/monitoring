3ad722be0018739e0816e516853e8de6bdaf2843
Elliott Coleman <ejaycoleman@googlemail.com>
2020-05-26 12:39:01 +0100

    only return notifications for the user requesting tasks

--- backend/src/resolvers/Task.js
+++ backend/src/resolvers/Task.js
@@ -6,8 +6,9 @@ function author(parent, args, context) {
     return context.prisma.task({ id: parent.id }).author()
 }
 
-function notifications(parent, args, context) {
-    return context.prisma.task({ id: parent.id }).notifications()
+async function notifications(parent, args, {prisma, user}) {
+    const fullUser = await prisma.user({id: user.id})
+    return prisma.task({ id: parent.id }).notifications({where: {user: fullUser}})
 }
 
 module.exports = {
