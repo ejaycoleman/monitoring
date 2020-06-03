4d5a64590745955dea6f5ebfad2c7821ae5b5634
Elliott Coleman <ejaycoleman@googlemail.com>
2020-06-03 13:10:59 +0100

    Show snackbar when changing threshold preferences when not logged in

--- frontend/src/Status/Status.js
+++ frontend/src/Status/Status.js
@@ -20,6 +20,8 @@ import { useDispatch, useSelector } from 'react-redux'
 
 import gql from 'graphql-tag'
 
+import Snackbar from '@material-ui/core/Snackbar'
+
 const retrieveExecutionsSubscription = gql`
     subscription {
         newExecution {
@@ -36,6 +38,7 @@ export default function Status(props) {
 	const [mostRecentExecution, setMostRecentExecution] = React.useState(0);
 	const [modalOpen, setModalOpen] = React.useState(false)
 	const [tasks, setTasks] = React.useState([]);
+	const [snackBarErrorShow, setSnackBarErrorShow] = React.useState(false)
 
 	const { authed } = useSelector(state => state.isLogged)
 	const reduxTasks = useSelector(state => state.tasks)
@@ -135,11 +138,17 @@ export default function Status(props) {
 						icon={<WatchLaterIcon style={{color: determineChipColor(mostRecentExecution)}}/>} 
 						label={mostRecentExecution ? `Last received ${moment.unix(mostRecentExecution).fromNow()}` : 'Never Received'} 
 						style={{backgroundColor: 'white'}} 
-						onClick={() => setModalOpen(true)}
+						onClick={() => authed ? setModalOpen(true) : setSnackBarErrorShow(true)}
 					/>	
 					<Dialog open={modalOpen} onClose={() => setModalOpen(false)}>
 						<PreferencesModal preferences={props.userPreferences} setPreferences={props.setPreferences} close={() => setModalOpen(false)}/>
 					</Dialog>
+					<Snackbar
+						anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+						open={snackBarErrorShow}
+						onClose={() => setSnackBarErrorShow(false)}
+						message="⚠️ Login to change threshold preferences"
+					/>
 				</div>
 			</div>
 		</div>
