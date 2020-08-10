ce6ca913d46e9ff9d5ba7dd6581b099504dfc777
Elliott Coleman <ejaycoleman@googlemail.com>
2020-08-10 15:12:07 +0100

    close button on menu

--- frontend/src/Menu/Menu.js
+++ frontend/src/Menu/Menu.js
@@ -3,6 +3,10 @@ import { useRef, useEffect } from "react";
 
 import Paper from '@material-ui/core/Paper'
 
+
+import IconButton from '@material-ui/core/IconButton'
+import CancelIcon from '@material-ui/icons/Cancel';
+
 export default function Menu(props) {
     const { show, onClose } = props
     const wrapperRef = useRef(null);
@@ -26,7 +30,14 @@ export default function Menu(props) {
             top: 15, 
             right: 15
         }}>
-            <Paper elevation={3} style={{padding: 10, display: 'flex', justifyContent: 'start', flexDirection: 'column'}}>{props.children}</Paper>
+            <Paper elevation={3} style={{padding: 10, display: 'flex', alignItems: 'flex-start', flexDirection: 'column'}}>
+                <div style={{width: '100%', display: 'flex', justifyContent: 'flex-end'}}>
+                    <IconButton size="small" onClick={() => onClose()}>
+                        <CancelIcon fontSize="inherit" />
+                    </IconButton>
+                </div>
+                {props.children}
+            </Paper>
 		</div>
 	)
 }
